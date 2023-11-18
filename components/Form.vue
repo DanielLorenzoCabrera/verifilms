200675125b426528a48a332349b6a6ad4fe0acc0
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-18 15:54:36 +0000

    feat: created FormWrapper component feat: created Input component feat: created Form component styles: forms css file created & updated

---
+++ components/Form.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    const props = defineProps({
+    action: { type: Function, required: true },
+    title: { type: String, required: false },
+    submitLabel: { type: String, required: false, default: "send" },
+    });
+</script>
+
+<template>
+  <form>
+    <legend v-if="title">{{ title }}</legend>
+    <fieldset>
+      <slot></slot>
+    </fieldset>
+    <input type="button" @click="() => action" :value="submitLabel" />
+  </form>
+</template>
---
+++ components/Input.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps({
+  type: { required: false, type: String, default: "text" },
+  placeholder: { required: true, type: String },
+});
+</script>
+<template>
+  <input :type="type" :placeholder="placeholder" />
+</template>
