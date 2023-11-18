02cf7d02132d832872f2c81490877bba26766959
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-18 15:48:51 +0000

    refactor: modified Header component

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const props = defineProps({
+    logo: { type: String, default: "" },
+  });
+</script>
+
+<template>
+  <header>
+    <figure>
+      <nuxt-link to="/">
+        <img :src="logo" />
+      </nuxt-link>
+    </figure>
+    <slot />
+  </header>
+</template>
