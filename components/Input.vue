f4c24e3be9f73817d063a0ff4ab4be51ad87c526
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-19 15:03:06 +0000

    refactor: modified input component. Props were modified and so do the input event handler.

--- components/Input.vue
+++ components/Input.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ref } from "vue";
 const props = defineProps({
   type: { required: false, type: String, default: "text" },
   placeholder: { required: true, type: String },
+  keyup: { required: false, type: Function, default: () => {} },
 });
+
+const emit = defineEmits(["update"]);
+const input = ref(null);
+
+const update = (): void => emit("update", input.value);
 </script>
 <template>
-  <input :type="type" :placeholder="placeholder" />
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    v-model="input"
+    @input="update"
+  />
 </template>
