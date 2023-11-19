1e92669360129c591d416d9b3855201eea6152e7
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-19 15:15:53 +0000

    refactor: removed unnecessary prop for custom input

--- components/Input.vue
+++ components/Input.vue
@@ -3,14 +3,13 @@ import { ref } from "vue";
 const props = defineProps({
   type: { required: false, type: String, default: "text" },
   placeholder: { required: true, type: String },
-  keyup: { required: false, type: Function, default: () => {} },
 });
 
 const emit = defineEmits(["update"]);
 const input = ref(null);
-
 const update = (): void => emit("update", input.value);
 </script>
+
 <template>
   <input
     :type="type"
