7d2fd8ee480ed3b486362714fffe09b2a52390b7
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-19 13:06:57 +0000

    feat: installed pinia & @pinia/nuxt pluggin as a store for the app feat: created & configured auth store. Add register and login methods refactor: created Credential type in types/User file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,5 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
-
+  modules: ["@pinia/nuxt"],
 });
---
+++ stores/auth.ts
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import type { Credential } from "~/types/User";
+
+interface Auth {
+  isLoggedIn: Boolean;
+  currentCredentials: Credential;
+  registeredUsers: Array<Credential>;
+}
+
+export const useAuthStore = defineStore("Auth", {
+  state: () =>
+    ({
+      isLoggedIn: false,
+      currentCredentials: { email: "", password: "" },
+      registeredUsers: [],
+    } as Auth),
+  actions: {
+    setEmail(email: String) {
+      const currentCredentials: Credential = this.currentCredentials;
+      this.currentCredentials = { ...currentCredentials, email };
+    },
+    setPassword(password: String) {
+      const currentCredentials: Credential = this.currentCredentials;
+      this.currentCredentials = { ...currentCredentials, password };
+    },
+    isUserRegistered(credential: Credential) {
+      const registeredUsers: Array<Credential> = this.registeredUsers;
+      return registeredUsers.some(
+        (registeredUser: Credential) =>
+          registeredUser.email === credential.email
+      );
+    },
+    login(credential: Credential) {
+      const isUserRegistered: Boolean = this.isUserRegistered(credential);
+      if (isUserRegistered) {
+        this.currentCredentials = { ...credential };
+        this.isLoggedIn = true;
+      }
+      return console.log("the user is not registered");
+    },
+    register(credential: Credential) {
+      const isUserRegistered: Boolean = this.isUserRegistered(credential);
+      if (isUserRegistered) return console.log("this account already exists");
+      this.registeredUsers = [...this.registeredUsers, credential];
+    },
+  },
+});
