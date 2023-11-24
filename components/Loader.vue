405651ed36f589fc5f1909310ad833a53395ccc5
Daniel Lorenzo <daniel@xrealityfactory.com>
2023-11-24 20:28:17 +0000

    refactor: Loader component created refactor: added Loader to CardDisplayer component to display when waiting for the items refactor: added default message when there are no items to CardDisplayer

---
+++ components/Loader.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import spiner from "~/assets/img/spinner_dots.svg";
+</script>
+<template>
+  <figure class="loader">
+    <img :src="spiner" alt="spiner image" />
+  </figure>
+</template>
--- components/Media/CardDisplayer.vue
+++ components/Media/CardDisplayer.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 const props = defineProps({
-  loading: { type: Boolean, required: true },
+  loading: { type: Boolean, required: false, default: false },
+  noItemsMessage: {
+    type: String,
+    required: false,
+    default: "There is no records",
+  },
+  items: { type: Array<any>, required: false, default: () => [] },
 });
 </script>
 <template>
-    <article class="card-displayer">
-        <slot></slot>
-    </article>
+  <article class="card-displayer">
+    <Loader v-if="loading" />
+    <template v-else>
+      <slot></slot>
+      <span v-if="items.length === 0" class="no-items">
+        {{ noItemsMessage }}
+      </span>
+    </template>
+  </article>
 </template>
